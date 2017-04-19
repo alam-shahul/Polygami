0616746ce80735de94765b9e8df3a613f76fcbbd
Shahul Alam <alam.shahul@gmail.com>
2017-04-19 10:16:31 -0400

    creaseGrid outlines extruded faces in orange.

--- algorithms/creaseGrid.js
+++ algorithms/creaseGrid.js
@@ -8,14 +8,51 @@ function creaseGrid(grid) {
   var corner;
   var currentX;
   var currentY;
-  for(var i = 0; i < grid.corners.length; i++) {
-    corner = grid.corners[i];
-    currentX = corner.tip.x;
-    currentY = corner.tip.y;
-    while(currentX % grid.w !== 0 && currentY % grid.h !== 0) {
-      
+
+  // Go through corners in priority order
+  for(var currPriority = 1; currPriority <= grid.corners.length/4; currPriority ++) {
+    // Make dictionary of Corners with priority = currPriority
+    var currCorners = {}
+    for(var j in grid.corners) {
+      if(j.priority === currPriority) {
+        currCorners[Point.toString(j.tip.x, j.tip.y)] = j;
+      }
+    }
+    
+    // Iterate through Corners of priority = currPriority
+    for(var i = 0; i < currCorners.length; i++) {
+      corner = currCorners[i];
+      // Add outline of extruded faces
+      if (corner.direction === 0) {
+        grid.addCrease(new Point(corner.tip.x, corner.tip.y), new Point(corner.tip.x-1, corner.tip.y), "orange");
+        grid.addCrease(new Point(corner.tip.x, corner.tip.y), new Point(corner.tip.x, corner.tip.y-1), "orange");
+      }
+      else if (corner.direction === 1) {
+        grid.addCrease(new Point(corner.tip.x, corner.tip.y), new Point(corner.tip.x-1, corner.tip.y), "orange");
+        grid.addCrease(new Point(corner.tip.x, corner.tip.y), new Point(corner.tip.x, corner.tip.y+1), "orange");
+      }
+      else if (corner.direction === 2) {
+        grid.addCrease(new Point(corner.tip.x, corner.tip.y), new Point(corner.tip.x+1, corner.tip.y), "orange");
+        grid.addCrease(new Point(corner.tip.x, corner.tip.y), new Point(corner.tip.x, corner.tip.y-1), "orange");
+      }
+      else if (corner.direction === 3) {
+        grid.addCrease(new Point(corner.tip.x, corner.tip.y), new Point(corner.tip.x+1, corner.tip.y), "orange");
+        grid.addCrease(new Point(corner.tip.x, corner.tip.y), new Point(corner.tip.x, corner.tip.y+1), "orange");
+      }
+
+      // Draw outside mountains
+      currentX = corner.tip.x;
+      currentY = corner.tip.y;
+      while(currentX % grid.w !== 0 && currentY % grid.h !== 0) {
+        if (corner.direction === 0) {
+        }
+        else if (corner.direction === 1) {
+        }
+        else if (corner.direction === 2) {
+        }
+        else if (corner.direction === 3) {
+        }
+      }
     }
   }
 }
-
-creaseGrid(new Grid(10,10));
